a99c23f387a0467664d983532ecd727d5ab861b3
Constantin Vassilyev <ange1rose666@icloud.com>
2025-01-10 15:18:25 +0500

    olya added

--- main.go
+++ main.go
@@ -4,10 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
-	var base int
-	base = 47
-	fmt.Printf("base: %d\n", base)
-	fmt.Println("vadim gay")
-	fmt.Println("beka gay")
-	fmt.Println("beka ffgay")
+	fmt.Println("hello olya")
+
 }
